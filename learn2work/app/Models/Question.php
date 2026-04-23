ba5501781519b0fe418d84e9286ebca330ddee00
briyanmm <binfulanq@gmail.com>
2026-04-23 08:48:57 +0700

    SCRUM-13 PBI #11 - Update kuis pilihan ganda, essay, feedback essay
    git commit -m "SCRUM-13 PBI #11 - Update kuis pilihan ganda, essay, feedback essay"

--- learn2work/app/Models/Question.php
+++ learn2work/app/Models/Question.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $fillable = ['quiz_id', 'type', 'question_text', 'options', 'correct_answer', 'feedback'];
+    protected function casts(): array {
+        return [
+            'options' => 'array',
+        ];
+    }
+    public function quiz() { return $this->belongsTo(Quiz::class); }
 }
